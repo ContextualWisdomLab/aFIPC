6e5ab59918e42c26774f4599b9b00dd2e1f3b22e
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-08-11 17:07:40 +0000

    ⚡ Bolt: factor 생성 및 데이터 프레임 열 할당 최적화
    * as.factor 대신 명시적인 levels와 함께 factor()를 사용하여 O(N) 레벨 자동 추론을 방지
    * data.frame(df, col) 복사 오버헤드를 막기 위해 $ 연산자로 직접 데이터 프레임 컬럼 할당
    * 테스트 코드 보완 및 CI 의존성 설치 추가

--- test_matrix_assignment.R
+++
@@ -1,4 +0,0 @@
-IPDData <- data.frame(matrix(rnorm(10), 10, 1))
-IPDgroup <- factor(rep(c('old', 'new'), c(5,5)), levels=c('old','new'))
-IPDData$IPDgroup <- IPDgroup
-print(IPDData)
--- test_perf_matrix2.R
+++
@@ -1,3 +0,0 @@
-df <- data.frame(A=1:3, B=4:6)
-df$C <- 7:9
-print(class(df))
